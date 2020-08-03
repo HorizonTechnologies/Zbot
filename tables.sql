2c15502a3e3afdb7720e69f61e5e228b70e1a1b2
IamTK <64881494+TharunKumar26@users.noreply.github.com>
2020-08-03 11:45:20 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,12 +1,10 @@
 
-create table resources(
-id serial primary key,
-title varchar(30),
-resource varchar(200),
-ts timestamp
-
-
-
+CREATE TABLE public.standup1
+(
+    _id serial,
+    slack_id varchar(20),
+    report json NOT NULL,
+    ts timestamp 
 )
 
 
